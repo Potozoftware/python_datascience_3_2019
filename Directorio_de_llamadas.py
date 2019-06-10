8956cd71d007573f22b891a04907a4cea4221705
Potozoftware <42122952+Potozoftware@users.noreply.github.com>
2019-06-10 09:31:30 -0600

    Create Directorio_de_llamadas.py
    Proyecto semana 1

---
+++ Directorio_de_llamadas.py
@@ -0,0 +1,24 @@
+import random
+from datetime import datetime, timedelta
+amigos = ['juan', 'carlos', 'ana']
+directory = []
+
+
+def get_rnd_date(start, end, fmt):
+
+  s = datetime.strptime(start, fmt)
+  e = datetime.strptime(end, fmt)
+
+  delta = e - s
+
+  return s + timedelta(days=(random.random() * delta.days))
+
+
+for x in range(10):
+    temp = []
+    temp.append(get_rnd_date("01/01/2019", "15/06/2019", "%d/%m/%Y").date())
+    temp.append(random.choice(amigos))
+    temp.append(random.randint(100, 1000))
+    directory.append(temp)
+
+print(*sorted(directory), sep = "\n")
